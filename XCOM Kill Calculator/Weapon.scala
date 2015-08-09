16d3a55fc90edb646f6646258c6ef81065c2fca6
Jeffry Orthober Jr <orthober.jeff@gmail.com>
2015-08-09 13:43:52 -0500

    Created by Coding Ground Client

---
+++ XCOM Kill Calculator/Weapon.scala
@@ -0,0 +1,6 @@
+case class Weapon(
+    chanceToHit: Double, //between 0.0 to 1.0
+    hitDamage: Double,   //integer >= 0
+    chanceToCrit: Double, //between 0.0 to 1.0
+    critDamage: Double  //integer >= 0
+    ){}
