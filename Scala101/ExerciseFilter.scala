8edb330a9b45c2f8f1f022eee3b081f28e078d0c
Jeffry Orthober Jr <orthober.jeff@gmail.com>
2017-05-01 01:00:04 -0500

    Created by Coding Ground Client

---
+++ Scala101/ExerciseFilter.scala
@@ -0,0 +1,13 @@
+import scala.collection.immutable._
+
+object ExerciseFilter {
+    
+    def usingPredefinedPredicateMethod(printme: Boolean){
+        //TODO init list
+        //TODO do filter
+    }
+    
+    def isEven(input: Int) : Boolean={
+        input%2==0
+    }
+}
